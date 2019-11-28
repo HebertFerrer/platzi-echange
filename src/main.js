b700399d166d790259f20fb2bc8caadd8020e16f
Hebert Ferrer <hebertferrer1999@gmail.com>
2019-11-27 23:43:53 -0300

    Nuevos filtros llamados 'dollar' y 'percent' que como su nombre indican, formatean un numero a dolar y a porcentaje

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import '@/assets/css/tailwind.css';
 import router from '@/router.js';
 
+import { dollar, percent } from '@/filters.js';
+
+Vue.filter('dollar', dollar);
+Vue.filter('percent', percent);
 Vue.config.productionTip = false;
 
 new Vue({
